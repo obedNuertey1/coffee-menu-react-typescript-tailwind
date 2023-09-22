c468c4b1fc018dfc90f818e6a00828e23de8668e
Obed Nuertey <onuertey1997@gmail.com>
2023-09-22 13:19:59 +0000

    update: updated ui

--- cypress/component/app.cy.tsx
+++ cypress/component/app.cy.tsx
@@ -6,7 +6,7 @@ import '../../src/index.css'
 describe('test for coffee app', () => {
   it('coffee App', () => {
     cy.mount(<App />);
-    cy.get('#menu').should('exist');
+    cy.get('.menu').should('exist');
     cy.get('h1').should('have.attr', 'class', 'text-center');
     cy.get('h2').should('exist').and('have.attr', 'class', 'text-center');
   })
--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,27 @@ import './App.css'
 const App = ():JSX.Element => {
   return (
     <>
-      <div id="menu" className="w-72 bg-burlywood">
+      <div className="menu bg-burlywood w-80 ml-auto mr-auto">
         <main>
           <h1 className="text-center">CAMPER CAFE</h1>
           <p className="text-center">Est. 2020</p>
           <section>
-            <h2 className="text-center">Coffee</h2>
+            <h2 className="text-center font-bold">Coffee</h2>
+            <article className="block pl-3 pr-3">
+              <p className="text-left inline-block w-1/2">French Vanilla</p><p className="text-right inline-block w-1/2">3.00</p>
+            </article>
+            <article className="block pl-3 pr-3">
+              <p className="text-left inline-block w-1/2">Caramel Macchiato</p><p className="text-right inline-block w-1/2">3.75</p>
+            </article>
+            <article className="block pl-3 pr-3">
+              <p className="text-left inline-block w-1/2">Pumpkin Spice</p><p className="text-right inline-block w-1/2">3.75</p>
+            </article>
+            <article className="block pl-3 pr-3">
+              <p className="text-left inline-block w-1/2">Hazelnut</p><p className="text-right inline-block w-1/2">4.00</p>
+            </article>
+            <article className="block pl-3 pr-3">
+              <p className="text-left inline-block w-1/2">Mocha</p><p className="text-right inline-block w-1/2">4.50</p>
+            </article>
           </section>
         </main>
       </div>

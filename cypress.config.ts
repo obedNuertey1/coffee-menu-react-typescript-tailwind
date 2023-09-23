0d3f158fb3ca816cfb7fff53c84e958d913e92c8
Obed Nuertey <onuertey1997@gmail.com>
2023-09-23 17:20:30 +0000

    feat: updated App.tsx

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: '3tf45q',
   component: {
     devServer: {
       framework: "react",
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,34 @@ import './App.css'
 import coffeCup from "./assets/image.png";
 import dessert from "./assets/image2.png";
 
+// How the foodstuffs object should be
+interface Ifoodstuff<T>{
+  name: String;
+  price: T;
+}
+// Array of Coffee flavors
+const firstSection:Ifoodstuff<string>[] = [{name: "French Vanilla", price: "3.00"}, {name: "Caramel Macchiato", price: "3.75"}, {name: "Pumpkin Spice", price: "3.75"}, {name: "Hazelnut", price: "4.00"}, {name: "Mocha", price: "4.50"}]
+
+// Array of desserts
+const secondSection:Ifoodstuff<string>[] = [{name: "Donut", price: "1.50"}, {name: "Cherry Pie", price: "2.75"}, {name: "Cheesecake", price:"3.00"}, {name: "Cinnamon Roll", price: "2.50"}];
+
+// Generate foodStuffs and their prices in jsx article format
+function generateFoods(itemList:Ifoodstuff<string>[]):JSX.Element[]{
+  return itemList.map((elem:Ifoodstuff<string>)=>{
+    return (
+      <article className="block">
+        <p className="mt-1 mb-1 text-left inline-block w-1/2">{elem.name}</p><p className="mt-1 mb-1 text-right inline-block w-1/2">{elem.price}</p>
+      </article>
+    );
+  }); 
+}
+
+
 const App = ():JSX.Element => {
+
+  const coffee:JSX.Element[] = generateFoods(firstSection);
+  const myDessert:JSX.Element[] = generateFoods(secondSection);
+
   return (
     <>
       <div className="menu selection:bg-brown selection:text-white bg-burlywood shadow-gray-950 shadow-md w-80 mt-5 mr-auto ml-auto rounded opacity-90 p-5 max-w-lg">
@@ -13,38 +40,12 @@ const App = ():JSX.Element => {
           <section>
             <h2 className="text-3xl font-impact text-center font-bold">Coffee</h2>
             <img src={`${coffeCup}`} alt="coffee Icon" className="block ml-auto mr-auto" />
-            <article className="block">
-              <p className="mt-1 mb-1 text-left inline-block w-1/2">French Vanilla</p><p className="mt-1 mb-1 text-right inline-block w-1/2">3.00</p>
-            </article>
-            <article className="block">
-              <p className="mt-1 mb-1 text-left inline-block w-1/2">Caramel Macchiato</p><p className="mt-1 mb-1 text-right inline-block w-1/2">3.75</p>
-            </article>
-            <article className="block">
-              <p className="mt-1 mb-1 text-left inline-block w-1/2">Pumpkin Spice</p><p className="mt-1 mb-1 text-right inline-block w-1/2">3.75</p>
-            </article>
-            <article className="block">
-              <p className="mt-1 mb-1 text-left inline-block w-1/2">Hazelnut</p><p className="mt-1 mb-1 text-right inline-block w-1/2">4.00</p>
-            </article>
-            <article className="block">
-              <p className="mt-1 mb-1 text-left inline-block w-1/2">Mocha</p><p className="mt-1 mb-1 text-right inline-block w-1/2">4.50</p>
-            </article>
+            {coffee}
           </section>
           <section>
             <h2 className="text-3xl font-impact font-bold text-center">Desserts</h2>
             <img src={dessert} alt="pie icon" className="block ml-auto mr-auto" />
-            <article className="item block">
-              <p className="mt-1 mb-1 dessert text-left inline-block w-1/2">Donut</p><p className="mt-1 mb-1 price text-right inline-block w-1/2">1.50</p>
-            </article>
-            <article className="item block" >
-              <p className="mt-1 mb-1 dessert text-left inline-block w-1/2">Cherry Pie</p><p className="mt-1 mb-1 price text-right inline-block w-1/2">2.75</p>
-            </article>
-            <article className="item block">
-              <p className="mt-1 mb-1 dessert text-left inline-block w-1/2">Cheesecake</p><p className="mt-1 mb-1 dessert text-right inline-block w-1/2">3.00</p>
-            </article>
-            <article className="item block">
-              <p className="mt-1 mb-1 dessert text-left inline-block w-1/2">Cinnamon Roll</p>
-              <p className="mt-1 mb-1 dessert text-right inline-block w-1/2">2.50</p>
-            </article>
+            {myDessert}
           </section>
         </main>
         <hr className="h-1 bg-brown border-brown mt-6" />
